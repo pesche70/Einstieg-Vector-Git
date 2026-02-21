be04487a41640793549c5cd4cd10479d09415be3
pesche70 <Peter.Meier@bbz-sh.ch>
2026-02-21 10:59:16 +0100

    Pesche-einstieg

--- main.cpp
+++ main.cpp
@@ -14,11 +14,29 @@ int main() {
     vector<Studierende> students;
 
     // Füge 4 Studierende in die Liste students ein
+    students.push_back({"Max", 5.5});
+    students.push_back({"Hans", 4.5});
+    students.push_back({"Klaus", 3.5});
+    students.push_back({"Franz", 4});
 
     cout << "Pruefungsergebnisse:" << endl;
     // Gib alle Studierenden und die jeweilige Note aus
+    cout << "Anzahl Produkte: " << students.size() << "\n";
+    cout << "Produkte:\n";
+    for (size_t i = 0; i < students.size(); ++i)
+    {
+        cout << students[i].name << " Note: " << students[i].note << "\n";
 
     // Gib aus, ob die jeweilige Person bestanden hat oder nicht
 
+    if ( students[i].note <4)
+        {
+        cout << "Pruefung nicht bestanden" <<endl;
+        }
+    else
+    {
+        cout << "Pruefung  bestanden" <<endl;
+    }
+    }
     return 0;
 }
